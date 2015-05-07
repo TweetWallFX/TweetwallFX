00e08c479da70fc5005b4b328660d7f3699609fb
J.Michelberger <j.michelberger@gmail.com>
2015-05-07 21:04:55 +0200

    Switch to StopList class usage.

--- 2d/src/main/java/org/tweetwallfx/twod/TagTweets.java
+++ 2d/src/main/java/org/tweetwallfx/twod/TagTweets.java
@@ -3,7 +3,6 @@ package org.tweetwallfx.twod;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
