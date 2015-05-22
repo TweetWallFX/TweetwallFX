8b01394fb94bb18b65711ae35550282790917580
Martin Klähn <grazertwo@gmail.com>
2015-05-22 22:55:20 +0200

    findbugs fix

--- 2d/src/main/java/org/tweetwallfx/twod/TagTweets.java
+++ 2d/src/main/java/org/tweetwallfx/twod/TagTweets.java
@@ -111,7 +111,7 @@ public class TagTweets {
         }
     }
 
-    private class TweetsUpdateTask extends Task<Void> {
+    private static class TweetsUpdateTask extends Task<Void> {
 
         private final Pattern pattern = Pattern.compile("\\s+");
         private final TweetSetData tweetSetData;
