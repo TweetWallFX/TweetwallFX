d04d21fba7f277be14b27c53c86233527cf849ae
Patrick Reinhart <patrick.reinhart@bison-group.com>
2017-03-16 13:20:37 +0100

    Fixes #117

--- config/src/main/java/org/tweetwallfx/config/Configuration.java
+++ config/src/main/java/org/tweetwallfx/config/Configuration.java
@@ -1,12 +1,20 @@
 package org.tweetwallfx.config;
 
+import static java.nio.file.Files.isRegularFile;
+import static java.nio.file.Files.newInputStream;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Enumeration;
+import java.util.List;
 import java.util.Objects;
 import java.util.Properties;
+
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 
@@ -29,24 +37,24 @@ public final class Configuration {
 
         try {
             final Enumeration<URL> resources = ClassLoader.getSystemClassLoader().getResources("config.properties");
-
             while (resources.hasMoreElements()) {
                 final URL url = resources.nextElement();
                 LOGGER.info("Found config file: " + url);
-
                 try (final InputStream is = url.openStream()) {
                     props.load(is);
                 }
             }
             
-            final File overrideFile = new File("config.properties");
-
-            if (overrideFile.isFile()) {
-                try (final InputStream is = overrideFile.toURI().toURL().openStream()) {
-                    props.load(is);
+            final List<Path> overrideFiles =
+                    Arrays.asList(Paths.get(System.getProperty("user.home"), "config.properties"),
+                            Paths.get("config.properties"));
+            for (Path overrideFile : overrideFiles) {
+                if (isRegularFile(overrideFile)) {
+                    LOGGER.info("Found config override file: " + overrideFile.toAbsolutePath());
+                    try (InputStream is = newInputStream(overrideFile)) {
+                        props.load(is);
+                    }
                 }
-            } else {
-                LOGGER.info("config override file ('" + overrideFile.getAbsolutePath() + "') found.");
             }
         } catch (final IOException ioe) {
             throw new RuntimeException("Error initiating Configuration", ioe);
