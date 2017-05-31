dbff4481533316df3ee0089cc132558c82cf6aa0
Patrick Reinhart <patrick@reini.net>
2017-05-31 21:06:42 +0200

    Implements #115 using Ehcache for mosaic images (#121)
    * Adds ehcache based image cache
    
    * Adds cache persistence and proper close on VM close
    
    * Adds slf4j log4j binding to get ehcache log entries

--- controls/src/main/java/org/tweetwallfx/controls/dataprovider/ImageMosaicDataProvider.java
+++ controls/src/main/java/org/tweetwallfx/controls/dataprovider/ImageMosaicDataProvider.java
@@ -3,29 +3,23 @@
  *
  * Copyright 2014-2016 TweetWallFX
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package org.tweetwallfx.controls.dataprovider;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.time.Instant;
@@ -39,12 +33,14 @@ import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import javafx.concurrent.Task;
-import javafx.scene.image.Image;
+
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.tweetwallfx.tweet.api.Tweet;
 import org.tweetwallfx.tweet.api.TweetStream;
+
+import javafx.concurrent.Task;
+import javafx.scene.image.Image;
 
 /**
  *
@@ -54,16 +50,19 @@ public class ImageMosaicDataProvider implements DataProvider {
 
     private static final Logger log = LogManager.getLogger(ImageMosaicDataProvider.class);
 
+    private final MediaCache cache;
+
     private Executor imageLoader = Executors.newSingleThreadExecutor(r -> {
-            Thread t = new Thread(r);
-            t.setName("Image-Downloader");
-            t.setDaemon(true);
-            return t;
-        });
+        Thread t = new Thread(r);
+        t.setName("Image-Downloader");
+        t.setDaemon(true);
+        return t;
+    });
 
     private List<ImageStore> images = new CopyOnWriteArrayList<>();
 
     public ImageMosaicDataProvider(TweetStream tweetStream) {
+        cache = MediaCache.INSTANCE;
         tweetStream.onTweet(tweet -> processTweet(tweet));
     }
 
@@ -72,8 +71,7 @@ public class ImageMosaicDataProvider implements DataProvider {
         if (null == tweet.getMediaEntries() || tweet.isRetweet()) {
             return;
         }
-        Arrays.stream(tweet.getMediaEntries())
-                .filter(me -> me.getType().equals("photo"))
+        Arrays.stream(tweet.getMediaEntries()).filter(me -> me.getType().equals("photo"))
                 .forEach(me -> {
                     String url;
                     switch (me.getSizes().keySet().stream().max(Comparator.naturalOrder()).get()) {
@@ -92,7 +90,7 @@ public class ImageMosaicDataProvider implements DataProvider {
                         default:
                             throw new RuntimeException("Illegal value");
                     }
-                    addImage(tweet, me.getId(), me.getId() + me.getMediaUrl().substring(me.getMediaUrl().lastIndexOf(".")), url, tweet.getCreatedAt());
+                    addImage(tweet, me.getId(), url, tweet.getCreatedAt());
                 });
     }
 
@@ -100,21 +98,19 @@ public class ImageMosaicDataProvider implements DataProvider {
         return Collections.<ImageStore>unmodifiableList(images);
     }
 
-    private void addImage(Tweet tweet, long mediaId, String fileName, String url, Date date) {
-
+    private void addImage(Tweet tweet, long mediaId, String url, Date date) {
         Task<Optional<ImageStore>> task = new Task<Optional<ImageStore>>() {
             @Override
             protected Optional<ImageStore> call() throws Exception {
-                File file = new File("dump/" + fileName);
-                if (file.exists()) {
+                if (cache.hasCachedMedia(mediaId)) {
                     return Optional.empty();
                 }
-                boolean mkdirs = file.getParentFile().mkdirs();
-                if (mkdirs) {
-                    log.info("directory for dumping files created!");
+                try (InputStream in = new URL(url).openStream()) {
+                    CachedMedia image = new CachedMedia(in);
+                    cache.putCachedMedia(mediaId, image);
+                    return Optional.of(new ImageStore(tweet, new Image(image.getInputStream()),
+                            date.toInstant(), mediaId));
                 }
-                downloadContent(new URL(url), file);
-                return Optional.of(new ImageStore(tweet, new Image(file.toPath().toUri().toURL().toExternalForm()), file, date.toInstant(), mediaId));
             }
         };
 
@@ -122,52 +118,31 @@ public class ImageMosaicDataProvider implements DataProvider {
             task.getValue().ifPresent(images::add);
             if (40 < images.size()) {
                 images.sort(Comparator.comparing(ImageStore::getInstant));
-                ImageStore removeLast = images.remove(images.size()-1);
-                removeLast.file.delete();
+                ImageStore removeLast = images.remove(images.size() - 1);
             }
         });
 
         imageLoader.execute(task);
     }
 
-    private static void downloadContent(URL url, File file) {
-        file.deleteOnExit();
-        if (file.getParentFile().mkdirs()) {
-            log.info("directory created " + file.getPath());
-        }
-
-        try (InputStream inputStream = url.openStream();
-                FileOutputStream outputStream = new FileOutputStream(file)) {
-            int read = 0;
-            byte[] bytes = new byte[1024];
-
-            while ((read = inputStream.read(bytes)) != -1) {
-                outputStream.write(bytes, 0, read);
-            }
-        } catch (IOException exception) {
-        }
-    }
-
     @Override
     public String getName() {
         return "MosaicDataProvider";
     }
-    
+
     public static class ImageStore {
         private final Tweet tweet;
         private final Image image;
         private final Instant instant;
-        private final File file;
         private final long mediaId;
 
-        public ImageStore(Tweet tweet, Image image, File file, Instant instant, long mediaId) {
+        public ImageStore(Tweet tweet, Image image, Instant instant, long mediaId) {
             this.tweet = tweet;
             this.image = image;
-            this.file = file;
-            this.instant = instant;            
+            this.instant = instant;
             this.mediaId = mediaId;
         }
-        
+
         public Instant getInstant() {
             return instant;
         }
@@ -179,7 +154,7 @@ public class ImageMosaicDataProvider implements DataProvider {
         public Image getImage() {
             return image;
         }
-        
+
         public long getMediaId() {
             return mediaId;
         }
@@ -190,7 +165,6 @@ public class ImageMosaicDataProvider implements DataProvider {
             hash = 97 * hash + Objects.hashCode(this.tweet);
             hash = 97 * hash + Objects.hashCode(this.image);
             hash = 97 * hash + Objects.hashCode(this.instant);
-            hash = 97 * hash + Objects.hashCode(this.file);
             hash = 97 * hash + (int) (this.mediaId ^ (this.mediaId >>> 32));
             return hash;
         }
@@ -219,12 +193,8 @@ public class ImageMosaicDataProvider implements DataProvider {
             if (!Objects.equals(this.instant, other.instant)) {
                 return false;
             }
-            if (!Objects.equals(this.file, other.file)) {
-                return false;
-            }
             return true;
         }
-
     }
 
 }
---
+++ controls/src/main/java/org/tweetwallfx/controls/dataprovider/MediaCache.java
@@ -0,0 +1,56 @@
+package org.tweetwallfx.controls.dataprovider;
+
+import java.net.URL;
+import java.util.EnumSet;
+import java.util.Optional;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.event.CacheEventListener;
+import org.ehcache.event.EventFiring;
+import org.ehcache.event.EventOrdering;
+import org.ehcache.event.EventType;
+import org.ehcache.xml.XmlConfiguration;
+
+public enum MediaCache {
+    INSTANCE;
+
+    private final Logger log;
+    private final CacheManager cacheManager;
+    private final Cache<Long, CachedMedia> imageCache;
+
+    private MediaCache() {
+        URL myUrl = getClass().getResource("MediaCache.xml");
+        XmlConfiguration xmlConfig = new XmlConfiguration(myUrl);
+        log = LogManager.getLogger(MediaCache.class);
+        cacheManager = CacheManagerBuilder.newCacheManager(xmlConfig);
+        Runtime.getRuntime().addShutdownHook(new Thread(cacheManager::close, "cache-shutdown"));
+        cacheManager.init();
+        // init caches
+        imageCache = cacheManager.getCache("mediaCache", Long.class, CachedMedia.class);
+        CacheEventListener<? super Object, ? super Object> logCacheEvent = event -> {
+            log.debug("Media id: " + event.getKey() + " - " + event.getType());
+        };
+        imageCache.getRuntimeConfiguration().registerCacheEventListener(logCacheEvent,
+                EventOrdering.UNORDERED, EventFiring.ASYNCHRONOUS, EnumSet.allOf(EventType.class));
+    }
+
+    public boolean hasCachedMedia(long mediaId) {
+        if (imageCache.containsKey(Long.valueOf(mediaId))) {
+            log.debug("Media id: " + mediaId + " - exists in cache");
+            return true;
+        }
+        return false;
+    }
+
+    public Optional<CachedMedia> getCachedMedia(long mediaId) {
+        return Optional.ofNullable(imageCache.get(Long.valueOf(mediaId)));
+    }
+
+    public void putCachedMedia(long mediaId, CachedMedia image) {
+        imageCache.put(Long.valueOf(mediaId), image);
+    }
+}
