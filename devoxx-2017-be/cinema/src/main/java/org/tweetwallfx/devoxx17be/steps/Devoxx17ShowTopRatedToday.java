259b1382b03a6f5291e5d294f087213f26639809
Sven Reimers <svenreimers@users.noreply.github.com>
2017-11-05 00:39:04 +0100

    Initial stab at Devoxx 2017 cinema wall (see #168) (#218)
    * Initial stab at Devoxx 2017 cinema wall
    
    * Fix licenses and comments, some cleanup
    
    * Fix licensing for XML based files

---
+++ devoxx-2017-be/cinema/src/main/java/org/tweetwallfx/devoxx17be/steps/Devoxx17ShowTopRatedToday.java
@@ -0,0 +1,119 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 TweetWallFX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tweetwallfx.devoxx17be.steps;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import javafx.animation.ParallelTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import org.apache.logging.log4j.LogManager;
+import org.tweetwall.devoxx.api.cfp.client.VotingResultTalk;
+import org.tweetwallfx.controls.WordleSkin;
+import org.tweetwallfx.controls.stepengine.AbstractStep;
+import org.tweetwallfx.controls.stepengine.StepEngine;
+import org.tweetwallfx.devoxx17be.animations.FlipInXTransition;
+import org.tweetwallfx.devoxx2017be.dataprovider.TopTalksTodayDataProvider;
+
+/**
+ * Devox 2017 Show Top Rated Talks Today (Flip In) Animation Step
+ * @author Sven Reimers
+ */
+public class Devoxx17ShowTopRatedToday extends AbstractStep {
+
+    @Override
+    public void doStep(StepEngine.MachineContext context) {
+        
+        WordleSkin wordleSkin = (WordleSkin) context.get("WordleSkin");
+        final TopTalksTodayDataProvider dataProvider = wordleSkin.getSkinnable().getDataProvider(TopTalksTodayDataProvider.class);
+        
+        List<FlipInXTransition> transitions = new ArrayList<>();
+        if (null == wordleSkin.getNode().lookup("#topRatedToday")) {
+            try {
+                Node scheduleNode = FXMLLoader.<Node>load(this.getClass().getResource("/topratedtalktoday.fxml"));
+                transitions.add(new FlipInXTransition(scheduleNode));
+                scheduleNode.setLayoutX(150);
+                scheduleNode.setLayoutY(200);
+                wordleSkin.getPane().getChildren().add(scheduleNode);
+                
+                GridPane grid = (GridPane) scheduleNode.lookup("#sessionGrid");
+                int col=0;
+                int row=0;
+                
+                Iterator<VotingResultTalk> iterator = dataProvider.getFilteredSessionData().iterator();
+                while (iterator.hasNext()) {
+                    Node node = createTalkNode(iterator.next());
+                    grid.getChildren().add(node);
+                    GridPane.setColumnIndex(node, col);
+                    GridPane.setRowIndex(node, row);
+                    row += 1;
+                }                
+                dataProvider.getFilteredSessionData();
+            } catch (IOException ex) {
+                LogManager.getLogger(Devoxx17ShowTopRatedToday.class.getName()).error(ex);
+            }
+        }
+        ParallelTransition flipIns = new ParallelTransition();
+        flipIns.getChildren().addAll(transitions);
+        flipIns.setOnFinished(e -> context.proceed());
+
+        flipIns.play();                        
+    }        
+    
+    private Node createTalkNode(VotingResultTalk votingResultTalk) {
+        try {
+            Node session = FXMLLoader.<Node>load(this.getClass().getResource("/ratedTalk.fxml"));
+            Text title = (Text)session.lookup("#title");
+            title.setText(votingResultTalk.getProposalTitle());
+            Text speakers = (Text)session.lookup("#speakers");
+            speakers.setText(votingResultTalk.getProposalsSpeakers());
+            Label averageVoting = (Label)session.lookup("#averageVote");
+            averageVoting.setText(String.format("%.1f", votingResultTalk.getRatingAverageScore()));
+            Label voteCount = (Label)session.lookup("#voteCount");
+            voteCount.setText(votingResultTalk.getRatingTotalVotes() + " Votes");
+            return session;
+        } catch (IOException ex) {
+            LogManager.getLogger(Devoxx17ShowSchedule.class).error(ex);
+            throw new RuntimeException(ex);
+        }
+    }    
+
+    @Override
+    public long preferredStepDuration(StepEngine.MachineContext context) {
+        return 1; // do not stop at this step;
+    }
+
+    @Override
+    public boolean shouldSkip(StepEngine.MachineContext context) {
+        WordleSkin wordleSkin = (WordleSkin) context.get("WordleSkin");
+        // if scheduleNode available do not execute Step.
+        return null != wordleSkin.getNode().lookup("#topRatedToday");
+    }
+    
+}
---
+++ devoxx-2017-be/cinema/src/main/java/org/tweetwallfx/devoxx2017be/dataprovider/ScheduleDataProvider.java
@@ -0,0 +1,77 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 TweetWallFX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tweetwallfx.devoxx2017be.dataprovider;
+
+import java.time.LocalDateTime;
+import java.time.OffsetTime;
+import java.time.format.TextStyle;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+import org.tweetwall.devoxx.api.cfp.client.CFPClient;
+import org.tweetwall.devoxx.api.cfp.client.Schedule;
+import org.tweetwallfx.controls.dataprovider.DataProvider;
+import org.tweetwallfx.tweet.api.TweetStream;
+
+/**
+ * DataProvider Implementation for Schedule Data
+ * @author Sven Reimers
+ */
+public class ScheduleDataProvider implements DataProvider {
+
+    private Schedule schedule;
+
+    private ScheduleDataProvider() {
+        updateSchedule();
+    }
+
+    private void updateSchedule() {        
+        String actualDayName = LocalDateTime.now().getDayOfWeek()
+                .getDisplayName(TextStyle.FULL, Locale.ENGLISH).toLowerCase(Locale.ENGLISH);
+        schedule = CFPClient.getClient().getSchedule(System.getProperty("org.tweetwalfx.devoxxbe17.day", actualDayName));
+    }
+
+    public List<SessionData> getFilteredSessionData() {        
+        String time = System.getProperty("org.tweetwalfx.devoxxbe17.time");
+        OffsetTime liveOffset = null != time ? OffsetTime.parse(time) : 
+                OffsetTime.now().minus(TimeZone.getDefault().getRawOffset(), ChronoUnit.MILLIS);
+        return  SessionData.from(schedule, liveOffset);
+    }
+
+    @Override
+    public String getName() {
+        return "Schedule-Devoxx2017BE";
+    }
+
+    public static class Factory implements DataProvider.Factory {
+
+        @Override
+        public DataProvider create(TweetStream tweetStream) {
+            return new ScheduleDataProvider();
+        }
+
+    }
+
+}
