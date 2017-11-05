f3bdc637a763b28452b92bb6109e7cf3ce8bda8d
Sven Reimers <svenreimers@users.noreply.github.com>
2017-11-05 12:51:56 +0100

    Add correct update behaviour for tweet stream display. (#232)
    * Add correct update behaviour for tweet stream display.
    -Created new DataProvider - fixes TweetWallFX/TweetWallFX#220
    -Respect Retweets - fixes TweetWallFX/TweetWallFX#226
    
    * Provide better comment for new data provider class

---
+++ devoxx-2017-be/cinema/src/main/java/org/tweetwallfx/devoxx2017be/dataprovider/TweetStreamDataProvider.java
@@ -0,0 +1,118 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014-2016 TweetWallFX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tweetwallfx.devoxx2017be.dataprovider;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Collectors;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.tweetwallfx.config.Configuration;
+import org.tweetwallfx.controls.dataprovider.DataProvider;
+import org.tweetwallfx.tweet.api.Tweet;
+import org.tweetwallfx.tweet.api.TweetQuery;
+import org.tweetwallfx.tweet.api.TweetStream;
+import org.tweetwallfx.tweet.api.Tweeter;
+
+/**
+ * Provides an always current list of tweets based on the configured query.
+ * The history length is not yet configurable.
+ * @author Sven Reimers
+ */
+public class TweetStreamDataProvider implements DataProvider {
+    
+    private static final Logger log = LogManager.getLogger(TweetStreamDataProvider.class);
+    private static final int HISTORY_SIZE = 5; 
+    private final ReadWriteLock tweetListLock = new ReentrantReadWriteLock();
+    private volatile Deque<Tweet> tweets = new ArrayDeque<>();
+    private final String searchText = Configuration.getInstance().getConfig("tweetwall.twitter.query");
+    
+    private TweetStreamDataProvider(TweetStream tweetStream) {
+        tweetStream.onTweet(tweet -> {
+            log.info("new Tweet received");
+                addTweet(tweet);
+        });
+        List<Tweet> history = getLatestHistory();
+        tweetListLock.writeLock().lock();
+        try {
+            history.forEach(this::addTweet);
+        } finally {
+            tweetListLock.writeLock().unlock();
+        }
+    }
+
+    private void addTweet(Tweet tweet) {
+        tweetListLock.writeLock().lock();   
+        try {
+            if (tweet.isRetweet()) {
+                Tweet originalTweet = tweet.getRetweetedTweet();
+                if(!tweets.stream().filter(twt -> originalTweet.getId() == twt.getId()).findAny().isPresent()) {
+                    tweets.addFirst(originalTweet);
+                }
+            } else {
+                tweets.addFirst(tweet);
+            }
+            if (tweets.size() > HISTORY_SIZE - 1) {
+                tweets.removeLast();
+            }
+        } finally {
+            tweetListLock.writeLock().unlock();
+        }
+    }
+    
+    public List<Tweet> getTweets() {
+        try {
+            tweetListLock.readLock().lock();
+            return new ArrayList<>(this.tweets);
+        } finally {
+            tweetListLock.readLock().unlock();
+        }
+    }
+
+    private List<Tweet> getLatestHistory() {
+        log.info("Reinit the history");
+        return Tweeter.getInstance().search(new TweetQuery()
+                        .query(searchText)
+                        .count(HISTORY_SIZE)).collect(Collectors.toList());        
+    }
+    
+    @Override
+    public String getName() {
+        return "TweetStream";
+    }
+
+    public static class Factory implements DataProvider.Factory {
+
+        @Override
+        public TweetStreamDataProvider create(TweetStream tweetStream) {
+            return new TweetStreamDataProvider(tweetStream);
+        }
+    
+    }
+    
+}
