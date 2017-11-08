565b04bc27d857a477e75f7a992ca37492e3b16f
Sven Reimers <sven.reimers@gmail.com>
2017-11-08 18:58:27 +0100

    Initial stab at showing the schedule in the exhibition hall. Postioning and sizing to be fine tuned. Fixes #166

---
+++ devoxx-2017-be/exhibition/src/main/java/org/tweetwallfx/devoxx17be/exhibition/Devoxx17ShowSchedule.java
@@ -0,0 +1,114 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 TweetWallFX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tweetwallfx.devoxx17be.exhibition;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import javafx.animation.ParallelTransition;
+import javafx.beans.binding.Bindings;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import org.apache.logging.log4j.LogManager;
+import org.tweetwallfx.controls.WordleSkin;
+import org.tweetwallfx.controls.stepengine.AbstractStep;
+import org.tweetwallfx.controls.stepengine.StepEngine;
+
+/**
+ * Devox 2017 Show Schedule (Flip In) Animation Step
+ * @author Sven Reimers
+ */
+public class Devoxx17ShowSchedule extends AbstractStep {
+
+    @Override
+    public void doStep(StepEngine.MachineContext context) {
+        
+        WordleSkin wordleSkin = (WordleSkin) context.get("WordleSkin");
+        final ScheduleDataProvider dataProvider = wordleSkin.getSkinnable().getDataProvider(ScheduleDataProvider.class);
+        
+        List<FlipInXTransition> transitions = new ArrayList<>();
+        if (null == wordleSkin.getNode().lookup("#scheduleNode")) {
+            try {
+                BorderPane scheduleNode = FXMLLoader.<BorderPane>load(this.getClass().getResource("/schedule.fxml"));
+                transitions.add(new FlipInXTransition(scheduleNode));
+                scheduleNode.layoutXProperty().bind(Bindings.multiply(0.5, Bindings.subtract(wordleSkin.getSkinnable().widthProperty(), scheduleNode.widthProperty())));
+                scheduleNode.layoutYProperty().bind(Bindings.multiply(0.2, Bindings.subtract(wordleSkin.getSkinnable().heightProperty(), scheduleNode.heightProperty())));
+                wordleSkin.getPane().getChildren().add(scheduleNode);
+                
+                GridPane grid = (GridPane) scheduleNode.lookup("#sessionGrid");
+                int col=0;
+                int row=0;
+                
+                Iterator<SessionData> iterator = dataProvider.getFilteredSessionData().iterator();
+                while (iterator.hasNext()) {
+                    Node node = createSessionNode(iterator.next());
+                    grid.getChildren().add(node);
+                    GridPane.setColumnIndex(node, col);
+                    GridPane.setRowIndex(node, row);
+                    col = (col == 0)? 1 : 0;
+                    if (col == 0 ) row++;
+                }
+            } catch (IOException ex) {
+                LogManager.getLogger(Devoxx17ShowSchedule.class.getName()).error(ex);
+            }
+        }
+        ParallelTransition flipIns = new ParallelTransition();
+        flipIns.getChildren().addAll(transitions);
+        flipIns.setOnFinished(e -> context.proceed());
+
+        flipIns.play();                
+        
+    }
+    
+    private Node createSessionNode(SessionData sessionData) {
+        try {
+            Node session = FXMLLoader.<Node>load(this.getClass().getResource("/session.fxml"));
+            Text title = (Text)session.lookup("#title");
+            title.setText(sessionData.title);
+            Text speakers = (Text)session.lookup("#speakers");
+            speakers.setText(sessionData.speakers.stream().collect(Collectors.joining(", ")));
+            Label room = (Label)session.lookup("#room");
+            room.setText(sessionData.room);
+            Label startTime = (Label)session.lookup("#startTime");
+            startTime.setText(sessionData.beginTime);
+            return session;
+        } catch (IOException ex) {
+            LogManager.getLogger(Devoxx17ShowSchedule.class).error(ex);
+            throw new RuntimeException(ex);
+        }
+    }
+
+    @Override
+    public Duration preferredStepDuration(StepEngine.MachineContext context) {
+        return Duration.ofSeconds(20);
+    }
+        
+}
