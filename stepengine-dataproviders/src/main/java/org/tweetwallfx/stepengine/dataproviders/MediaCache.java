6f6a3db9a4546e55da5aa37bb43609ac9982ec94
Martin Klähn <mklaehn@users.noreply.github.com>
2018-10-01 22:14:14 +0200

    Added/Corrected license header in *.java files. refs TweetWallFX/TweetwallFX#216

--- stepengine-dataproviders/src/main/java/org/tweetwallfx/stepengine/dataproviders/MediaCache.java
+++ stepengine-dataproviders/src/main/java/org/tweetwallfx/stepengine/dataproviders/MediaCache.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright 2016-2018 TweetWallFX
+ * Copyright 2018 TweetWallFX
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
