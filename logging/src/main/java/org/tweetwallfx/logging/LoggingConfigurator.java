a8c301a1e6002d6bc3c974ee66953f29baadde2a
Martin Klähn <mklaehn@users.noreply.github.com>
2018-10-03 23:50:16 +0200

    Introduced Logging project hiding the log4j-core dependency from compile configuration of consumers of that subproject. (#524)
    Moved LoggingConfigurator onto that project.
    fixes TweetWallFX/TweetwallFX#523

--- logging/src/main/java/org/tweetwallfx/logging/LoggingConfigurator.java
+++ logging/src/main/java/org/tweetwallfx/logging/LoggingConfigurator.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package org.tweetwallfx.config;
+package org.tweetwallfx.logging;
 
 import java.io.File;
 import java.util.concurrent.atomic.AtomicBoolean;
