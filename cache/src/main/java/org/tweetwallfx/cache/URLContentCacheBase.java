7bcb82697609f2a21cde6db0abd2dd0e014fc216
Martin Klähn <mklaehn@users.noreply.github.com>
2018-10-15 21:12:50 +0200

    Converter URLContentCache into easily extendable (by subclassing) class. Modified usage in ImageMosaicDataProvider. fixes TweetWallFX/TweetwallFX#538

---
+++ cache/src/main/java/org/tweetwallfx/cache/URLContentCacheBase.java
@@ -0,0 +1,238 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 TweetWallFX
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.tweetwallfx.cache;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import javafx.concurrent.Task;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.ehcache.Cache;
+import org.tweetwallfx.config.Configuration;
+
+/**
+ * Caches the content urlString to their urlString.
+ */
+public abstract class URLContentCacheBase {
+
+    private static final Logger LOG = LogManager.getLogger(URLContentCacheBase.class);
+    private static final ThreadGroup THREAD_GROUP = new ThreadGroup("URLContentCache");
+    private final String cacheName;
+    private final Executor contentLoader;
+    private final Cache<String, URLContent> urlContentCache;
+
+    protected URLContentCacheBase(final String cacheName) {
+        this.cacheName = cacheName;
+        urlContentCache = CacheManagerProvider.getCache(
+                cacheName,
+                String.class,
+                URLContent.class);
+        contentLoader = createExecutor(
+                Configuration.getInstance()
+                        .getConfigTyped(CacheSettings.CONFIG_KEY, CacheSettings.class)
+                        .getCaches()
+                        .get(cacheName)
+                        .getContentLoaderThreads(),
+                cacheName);
+    }
+
+    /**
+     * Determines if the cache - at this moment - has a cache entry for the
+     * {@code urlString}.
+     *
+     * @param urlString the URL of the content
+     *
+     * @return a boolean flag that if {@code true} indicates that the cache
+     * currently has an entry for the {@code urlString}
+     */
+    public final boolean hasCachedContent(final String urlString) {
+        if (urlContentCache.containsKey(urlString)) {
+            LOG.debug("{}: Content for '{}': - exists in cache", cacheName, urlString);
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Retrieves the current cache value of the cache entry with
+     * {@code urlString} as key wrapped in an Optional instance.
+     *
+     * @param urlString the URL of the content
+     *
+     * @return an {@link Optional} containing the current cache content for the
+     * {code urlString}
+     */
+    public final Optional<URLContent> getCachedContent(final String urlString) {
+        LOG.debug("{}: Getting Content for '{}'", cacheName, urlString);
+        return Optional.ofNullable(urlContentCache.get(urlString));
+    }
+
+    /**
+     * Retrieves the cached content asyncronuously for {code urlString} and
+     * passes it to {@code contentConsumer}. If no cached content exists the
+     * content loaded and cached and then passed to {@code contentConsumer}.
+     *
+     * @param urlString the string of the URL content to get
+     *
+     * @return an URLContent in case content was loaded or {@code null}
+     */
+    public final URLContent getCachedOrLoad(final String urlString) {
+        try {
+            return getCachedOrLoadSync(urlString);
+        } catch (IOException ex) {
+            LOG.error(cacheName + ": Failed to load content from " + urlString, ex);
+            return null;
+        }
+    }
+
+    /**
+     * Retrieves the cached content asyncronuously for {code urlString} and
+     * passes it to {@code contentConsumer}. If no cached content exists the
+     * content loaded and cached and then passed to {@code contentConsumer}.
+     *
+     * @param urlString the string of the URL content to get
+     *
+     * @param contentConsumer the Consumer processing the content
+     */
+    public final void getCachedOrLoad(final String urlString, final Consumer<URLContent> contentConsumer) {
+        Objects.requireNonNull(contentConsumer, "contentConsumer must not be null");
+        final Task<URLContent> task = new Task<URLContent>() {
+
+            @Override
+            protected URLContent call() throws Exception {
+                return getCachedOrLoadSync(urlString);
+            }
+        };
+
+        task.setOnSucceeded(event -> {
+            contentConsumer.accept(task.getValue());
+        });
+        task.setOnFailed(event -> {
+            LOG.error(cacheName + ": Failed to load content from " + urlString, task.getException());
+        });
+        contentLoader.execute(task);
+    }
+
+    private URLContent getCachedOrLoadSync(final String urlString) throws IOException {
+        URLContent urlc = urlContentCache.get(urlString);
+
+        if (null == urlc) {
+            urlc = new URLContent(urlString);
+            putCachedContent(urlString, urlc);
+        }
+
+        return urlc;
+    }
+
+    /**
+     * Adds the {@code content} to the cache under the {@code urlString} key.
+     *
+     * @param urlString the string of the URL to cache
+     *
+     * @param content the content to cache
+     */
+    public final void putCachedContent(final String urlString, final URLContent content) {
+        LOG.debug("{}: Setting Content for '{}'", cacheName, urlString);
+        urlContentCache.put(urlString, content);
+    }
+
+    /**
+     * Loads the content from {@code urlString} and adds that content to the
+     * cache under the key {@code urlString} in case loading the content
+     * succeeded.
+     *
+     * @param urlString the string of the URL to cache
+     */
+    public final void putCachedContent(final String urlString) {
+        putCachedContentAsync(urlString, null);
+    }
+
+    /**
+     * Puts the content of the provided {@code urlString} into the cache (should
+     * loading succeed).
+     *
+     * @param urlString the string of the URL to cache
+     *
+     * @param contentConsumer the Consumer processing the content
+     */
+    public final void putCachedContent(final String urlString, final Consumer<URLContent> contentConsumer) {
+        putCachedContentAsync(urlString, contentConsumer);
+    }
+
+    private void putCachedContentAsync(final String urlString, final Consumer<URLContent> contentConsumer) {
+        final Task<URLContent> task = new Task<URLContent>() {
+
+            @Override
+            protected URLContent call() throws Exception {
+                return new URLContent(urlString);
+            }
+        };
+
+        task.setOnSucceeded(event -> {
+            putCachedContent(urlString, task.getValue());
+
+            if (null != contentConsumer) {
+                contentConsumer.accept(task.getValue());
+            }
+        });
+        task.setOnFailed(event -> LOG.error(cacheName + ": Failed to load content from " + urlString, task.getException()));
+        contentLoader.execute(task);
+    }
+
+    public static URLContentCacheBase getDefault() {
+        return Default.INSTANCE;
+    }
+
+    private static Executor createExecutor(final int nrThreads, final String name) {
+        final AtomicInteger counter = new AtomicInteger();
+        final ThreadGroup threadGroup = new ThreadGroup(THREAD_GROUP, name);
+        final ThreadFactory threadFactory = r -> {
+            final Thread t = new Thread(threadGroup, r, "contentLoader" + counter.incrementAndGet());
+            t.setDaemon(true);
+            return t;
+        };
+
+        if (nrThreads < 2) {
+            return Executors.newSingleThreadExecutor(threadFactory);
+        } else {
+            return Executors.newFixedThreadPool(nrThreads, threadFactory);
+        }
+    }
+
+    private static final class Default extends URLContentCacheBase {
+
+        private static final Default INSTANCE = new Default();
+
+        public Default() {
+            super("default");
+        }
+    }
+}
