fda8113471443106d30705708e1d38b78c747bee
Martin Klähn <mklaehn@users.noreply.github.com>
2018-10-17 23:07:45 +0200

    Added Configuration to SpeakerImageProvider allowing for profile image stand-in in case profile image was not retrievable. fixes TweetWallFX/TweetwallFX#310

--- devoxx-cfp-stepengine/src/main/java/org/tweetwallfx/devoxx/cfp/stepengine/dataprovider/SpeakerImageProvider.java
+++ devoxx-cfp-stepengine/src/main/java/org/tweetwallfx/devoxx/cfp/stepengine/dataprovider/SpeakerImageProvider.java
@@ -24,12 +24,16 @@
 package org.tweetwallfx.devoxx.cfp.stepengine.dataprovider;
 
 import java.io.InputStream;
+import java.util.Collections;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Supplier;
 import javafx.scene.image.Image;
 import org.tweetwall.devoxx.api.cfp.client.CFPClient;
 import org.tweetwall.devoxx.api.cfp.client.Speaker;
 import org.tweetwall.devoxx.api.cfp.client.SpeakerReference;
+import static org.tweetwall.util.ToString.createToString;
+import static org.tweetwall.util.ToString.map;
 import org.tweetwallfx.stepengine.api.DataProvider;
 import org.tweetwallfx.stepengine.api.config.StepEngineSettings;
 import org.tweetwallfx.stepengine.dataproviders.ProfileImageCache;
@@ -39,8 +43,10 @@ import org.tweetwallfx.stepengine.dataproviders.ProfileImageCache;
  */
 public final class SpeakerImageProvider implements DataProvider {
 
-    private SpeakerImageProvider() {
-        // prevent instantiation
+    private final Config config;
+
+    private SpeakerImageProvider(final Config config) {
+        this.config = config;
     }
 
     public Image getSpeakerImage(final Speaker speaker) {
@@ -55,8 +61,19 @@ public final class SpeakerImageProvider implements DataProvider {
         final Supplier<InputStream> supplier = ProfileImageCache.INSTANCE.getCachedOrLoad(avatarURL);
 
         if (null == supplier) {
-            // use stand-in for non-loadable
-            return null; // TODO: fix with https://github.com/TweetWallFX/TweetwallFX/issues/310
+            // avatar url is not in cache (url content was not loadable)
+            // look for configured replacement
+            final String urlReplacement = config.getUrlReplacements().get(avatarURL);
+            Image image = null == urlReplacement
+                    ? null
+                    : getSpeakerImage(urlReplacement);
+
+            if (null == image) {
+                // use stand-in for non-loadable
+                image = new Image(Thread.currentThread().getContextClassLoader().getResourceAsStream(config.getNoImageResource()));
+            }
+
+            return image;
         } else {
             return new Image(supplier.get());
         }
@@ -69,6 +86,9 @@ public final class SpeakerImageProvider implements DataProvider {
                 .map(Speaker::getAvatarURL)
                 .filter(Objects::nonNull)
                 .forEach(urlString -> ProfileImageCache.INSTANCE.getCachedOrLoad(urlString, this::handleURLContent));
+
+        config.getUrlReplacements()
+                .forEach((k, v) -> ProfileImageCache.INSTANCE.putCachedContent(v, this::handleURLContent));
     }
 
     private void handleURLContent(final Supplier<InputStream> urlc) {
@@ -83,12 +103,44 @@ public final class SpeakerImageProvider implements DataProvider {
 
         @Override
         public SpeakerImageProvider create(final StepEngineSettings.DataProviderSetting dataProviderSetting) {
-            return new SpeakerImageProvider();
+            return new SpeakerImageProvider(dataProviderSetting.getConfig(Config.class));
         }
 
         @Override
         public Class<SpeakerImageProvider> getDataProviderClass() {
             return SpeakerImageProvider.class;
+        }
+    }
+
+    public static class Config {
+
+        private String noImageResource = "icons/user1-256x256.png";
+        private Map<String, String> urlReplacements = Collections.emptyMap();
+
+        public String getNoImageResource() {
+            return noImageResource;
+        }
+
+        public void setNoImageResource(final String noImageResource) {
+            this.noImageResource = noImageResource;
+        }
+
+        public Map<String, String> getUrlReplacements() {
+            return urlReplacements;
+        }
+
+        public void setUrlReplacements(final Map<String, String> urlReplacements) {
+            this.urlReplacements = null == urlReplacements || urlReplacements.isEmpty()
+                    ? Collections.emptyMap()
+                    : Collections.unmodifiableMap(urlReplacements);
+        }
+
+        @Override
+        public String toString() {
+            return createToString(this, map(
+                    "noImageResource", getNoImageResource(),
+                    "urlReplacements", getUrlReplacements()
+            )) + " extends " + super.toString();
         }
     }
 }
