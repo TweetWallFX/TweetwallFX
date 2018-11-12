19c11b41be8320cab2c98771e6a276cb2aa99d3f
Martin Klähn <mklaehn@users.noreply.github.com>
2018-11-12 20:01:08 +0100

    Made direct configuration of DataProviderSetting and FilterStepDefinition possible. Made ConfigurableObjectBase.getData(Class) overridable and doing exactly for the listed types. The overridden methods merge the configuration data for the step (in-place) and reachable via the FQN type name. fixes TweetWallFX/TweetwallFX#692

--- stepengine-api/src/main/java/org/tweetwallfx/stepengine/api/config/StepEngineSettings.java
+++ stepengine-api/src/main/java/org/tweetwallfx/stepengine/api/config/StepEngineSettings.java
@@ -25,13 +25,16 @@ package org.tweetwallfx.stepengine.api.config;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import org.tweetwallfx.config.Configuration;
 import org.tweetwallfx.config.ConfigurationConverter;
 import org.tweetwallfx.config.ConnectionSettings;
 import org.tweetwallfx.stepengine.api.DataProvider;
 import org.tweetwallfx.stepengine.api.Step;
 import org.tweetwallfx.stepengine.api.StepEngine;
 import org.tweetwallfx.util.ConfigurableObjectBase;
+import org.tweetwallfx.util.JsonDataConverter;
 import static org.tweetwallfx.util.ToString.createToString;
 import static org.tweetwallfx.util.ToString.map;
 
@@ -156,6 +159,11 @@ public final class StepEngineSettings {
      * {@link #getConfig()} or {@link #getConfig(java.lang.Class)}) for a
      * {@link DataProvider} instance (identified via
      * {@link #getDataProviderClassName()}.
+     *
+     * <p>
+     * Configuration can be extended by configuring the properties of the
+     * {@code config} section of this definition on the root level of the
+     * Configuration.
      */
     public static final class DataProviderSetting extends ConfigurableObjectBase {
 
@@ -178,6 +186,14 @@ public final class StepEngineSettings {
          */
         public void setDataProviderClassName(final String dataProviderClassName) {
             this.dataProviderClassName = dataProviderClassName;
+        }
+
+        @Override
+        public <T> T getConfig(final Class<T> typeClass) {
+            @SuppressWarnings("unchecked")
+            final Map<String, Object> specializedConfig = (Map<String, Object>) Configuration.getInstance().getConfig(typeClass.getName(), Collections.emptyMap());
+            final Map<String, Object> mergedConfig = Configuration.mergeMap(getConfig(), specializedConfig);
+            return JsonDataConverter.convertFromObject(mergedConfig, typeClass);
         }
 
         @Override
