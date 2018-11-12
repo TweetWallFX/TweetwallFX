6158018c1bf04eee3b9e3a2af5565652cfc584ba
Sven Reimers <svenreimers@users.noreply.github.com>
2018-11-12 17:01:32 +0100

    Add execption handling for step execution in case of terminal failure… (#686)
    * Add execption handling for step execution in case of terminal failure. see #683
    
    * Ensure step engine continues in case of exception. See #683-1

--- stepengine-api/src/main/java/org/tweetwallfx/stepengine/api/StepEngine.java
+++ stepengine-api/src/main/java/org/tweetwallfx/stepengine/api/StepEngine.java
@@ -243,9 +243,25 @@ public final class StepEngine {
             LOG.info("call {}.doStep()", stepToExecute.getClass().getSimpleName());
 
             if (stepToExecute.requiresPlatformThread()) {
-                Platform.runLater(() -> stepToExecute.doStep(context));
+                Platform.runLater(() -> {
+                    try {
+                        stepToExecute.doStep(context);
+                    } catch (RuntimeException | Error e) {
+                        LOG.fatal("StepExecution has terminal failure {} ", stepToExecute.getClass().getSimpleName());
+                        LOG.fatal("caused by", e);
+                        // enforce that animation continues
+                        context.proceed();
+                    }
+                });
             } else {
-                stepToExecute.doStep(context);
+                    try {
+                        stepToExecute.doStep(context);
+                    } catch (RuntimeException | Error e) {
+                        LOG.fatal("StepExecution has terminal failure {} ", stepToExecute.getClass().getSimpleName());
+                        LOG.fatal("caused by", e);
+                        // enforce that animation continues
+                        context.proceed();
+                    }
             }
 
             final long stop = System.currentTimeMillis();
