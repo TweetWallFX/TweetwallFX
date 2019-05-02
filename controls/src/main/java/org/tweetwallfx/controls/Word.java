4b483834ebfd491d812c0c452ff46ac7985a1362
Martin Klähn <mklaehn@users.noreply.github.com>
2019-05-02 21:14:35 +0200

    Update LICENSE and license headers. (#763)
    fixes TweetWallFX/TweetwallFX#762

--- controls/src/main/java/org/tweetwallfx/controls/Word.java
+++ controls/src/main/java/org/tweetwallfx/controls/Word.java
@@ -1,7 +1,7 @@
 /*
- * The MIT License
+ * The MIT License (MIT)
  *
- * Copyright 2015-2018 TweetWallFX
+ * Copyright (c) 2015-2019 TweetWallFX
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
