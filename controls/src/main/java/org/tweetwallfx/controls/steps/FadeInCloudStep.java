5181ed5c8c7384659036375c96d6c1d7487d47f9
Sven Reimers <sven.reimers@gmail.com>
2022-09-12 22:12:24 +0200

    #1237 Update FadeInCloudStep with enhanced customization

--- controls/src/main/java/org/tweetwallfx/controls/steps/FadeInCloudStep.java
+++ controls/src/main/java/org/tweetwallfx/controls/steps/FadeInCloudStep.java
@@ -28,11 +28,18 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
 import javafx.animation.SequentialTransition;
 import javafx.animation.Transition;
+import javafx.geometry.BoundingBox;
 import javafx.geometry.Bounds;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.effect.GaussianBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 import org.tweetwallfx.controls.Word;
@@ -42,34 +49,41 @@ import org.tweetwallfx.controls.dataprovider.TagCloudDataProvider;
 import org.tweetwallfx.stepengine.api.DataProvider;
 import org.tweetwallfx.stepengine.api.Step;
 import org.tweetwallfx.stepengine.api.StepEngine.MachineContext;
+import org.tweetwallfx.stepengine.api.config.AbstractConfig;
 import org.tweetwallfx.stepengine.api.config.StepEngineSettings;
 
 public class FadeInCloudStep implements Step {
 
-    private FadeInCloudStep() {
-        // prevent external instantiation
+    private final Config config;
+
+    private FadeInCloudStep(Config config) {
+        this.config = config;
     }
 
     @Override
     public java.time.Duration preferredStepDuration(final MachineContext context) {
-        return java.time.Duration.ofSeconds(5);
+        return java.time.Duration.ofMillis(config.getStepDuration());
+    }
+
+    @Override
+    public boolean shouldSkip(MachineContext context) {
+        List<Word> sortedWords = context.getDataProvider(TagCloudDataProvider.class).getWords();
+        return sortedWords.isEmpty();
     }
 
     @Override
     public void doStep(final MachineContext context) {
-        List<Word> sortedWords = context.getDataProvider(TagCloudDataProvider.class).getWords();
-
-        if (sortedWords.isEmpty()) {
-            return;
-        }
-
         WordleSkin wordleSkin = (WordleSkin) context.get("WordleSkin");
+        context.put("cloudConfig", config);
+        List<Word> sortedWords = context.getDataProvider(TagCloudDataProvider.class).getWords();
         List<Word> limitedWords = sortedWords.stream().limit(wordleSkin.getDisplayCloudTags()).toList();
-        limitedWords.sort(Comparator.reverseOrder());
+        List<Word> cutOfflLimitedWords = limitedWords.stream().
+                sorted(Comparator.reverseOrder()).
+                map(w -> new Word(w.getText().substring(0, Math.min(config.tagLength, w.getText().length())), w.getWeight())).toList();
 
-        Bounds layoutBounds = wordleSkin.getPane().getLayoutBounds();
+        Bounds layoutBounds = new BoundingBox(1,1, config.width, config.height);
 
-        WordleLayout.Configuration configuration = new WordleLayout.Configuration(limitedWords, wordleSkin.getFont(), wordleSkin.getFontSizeMax(), layoutBounds);
+        WordleLayout.Configuration configuration = new WordleLayout.Configuration(cutOfflLimitedWords, wordleSkin.getFont(), wordleSkin.getFontSizeMax(), layoutBounds);
         if (null != wordleSkin.getLogo()) {
             configuration.setBlockedAreaBounds(wordleSkin.getLogo().getBoundsInParent());
         }
@@ -83,18 +97,41 @@ public class FadeInCloudStep implements Step {
         List<Transition> moveTransitions = new ArrayList<>();
         List<Transition> fadeInTransitions = new ArrayList<>();
 
+        SnapshotParameters snapshotParameters = new SnapshotParameters();
+        snapshotParameters.setViewport(new Rectangle2D(
+                config.layoutX,
+                config.layoutY,
+                config.width,
+                config.height));
+
+        WritableImage writableImage = new WritableImage((int)config.width, (int) config.height);
+        wordleSkin.getNode().snapshot(snapshotParameters,
+                writableImage);
+
+        ImageView snapCopy = new ImageView(writableImage);
+        snapCopy.setLayoutX(config.layoutX);
+        snapCopy.setLayoutY(config.layoutY);
+        snapCopy.setEffect(new GaussianBlur());
+        snapCopy.setId("blurredCopy");
+
+        wordleSkin.getPane().getChildren().add(snapCopy);
+
+        FadeTransition ftSnapCopy = new FadeTransition(defaultDuration, snapCopy);
+        ftSnapCopy.setToValue(1);
+        fadeInTransitions.add(ftSnapCopy);
+
         cloudWordleLayout.getWordLayoutInfo().entrySet().stream().forEach(entry -> {
             Word word = entry.getKey();
             Bounds bounds = entry.getValue();
             Text textNode = cloudWordleLayout.createTextNode(word);
             wordleSkin.word2TextMap.put(word, textNode);
-            textNode.setLayoutX(bounds.getMinX() + layoutBounds.getWidth() / 2d);
-            textNode.setLayoutY(bounds.getMinY() + layoutBounds.getHeight() / 2d + bounds.getHeight() / 2d);
+            textNode.setLayoutX(config.layoutX + bounds.getMinX() + layoutBounds.getWidth() / 2d);
+            textNode.setLayoutY(config.layoutY + bounds.getMinY() + layoutBounds.getHeight() / 2d + Math.abs(bounds.getHeight()) / 2d);
             textNode.setOpacity(0);
             wordleSkin.getPane().getChildren().add(textNode);
-            FadeTransition ft = new FadeTransition(defaultDuration, textNode);
-            ft.setToValue(1);
-            fadeInTransitions.add(ft);
+            FadeTransition ftTextNode = new FadeTransition(defaultDuration, textNode);
+            ftTextNode.setToValue(1);
+            fadeInTransitions.add(ftTextNode);
         });
 
         ParallelTransition fadeOuts = new ParallelTransition();
@@ -117,7 +154,7 @@ public class FadeInCloudStep implements Step {
 
         @Override
         public FadeInCloudStep create(final StepEngineSettings.StepDefinition stepDefinition) {
-            return new FadeInCloudStep();
+            return new FadeInCloudStep(stepDefinition.getConfig(Config.class));
         }
 
         @Override
@@ -129,5 +166,13 @@ public class FadeInCloudStep implements Step {
         public Collection<Class<? extends DataProvider>> getRequiredDataProviders(final StepEngineSettings.StepDefinition stepSettings) {
             return Arrays.asList(TagCloudDataProvider.class);
         }
+    }
+
+    private static class Config extends AbstractConfig {
+        public double layoutX = 0;
+        public double layoutY = 0;
+        public double width = 0;
+        public double height = 0;
+        public int tagLength = 15;
     }
 }
