7d5dc5a0148af3332b81c22264e053ebb4423a01
Martin Klähn <mklaehn@users.noreply.github.com>
2022-10-11 17:10:09 +0200

    Fixed last errors in TopRatedTalks Steps (missing fxml files, wrong config data source)

--- conference-stepengine/src/main/java/org/tweetwallfx/conference/stepengine/dataprovider/TopTalksWeekDataProvider.java
+++ conference-stepengine/src/main/java/org/tweetwallfx/conference/stepengine/dataprovider/TopTalksWeekDataProvider.java
@@ -118,10 +118,10 @@ public final class TopTalksWeekDataProvider implements DataProvider, DataProvide
                 final ScheduleType scheduleType,
                 final Long initialDelay,
                 final Long scheduleDuration) {
-            if (nrVotes < 0) {
+            this.nrVotes = valueOrDefault(nrVotes, 5);
+            if (this.nrVotes < 0) {
                 throw new IllegalArgumentException("property 'nrVotes' must not be a negative number");
             }
-            this.nrVotes = valueOrDefault(nrVotes, 5);
             this.scheduleType = valueOrDefault(scheduleType, ScheduleType.FIXED_RATE);
             this.initialDelay = valueOrDefault(initialDelay, 0L);
             this.scheduleDuration = valueOrDefault(scheduleDuration, 300L);
