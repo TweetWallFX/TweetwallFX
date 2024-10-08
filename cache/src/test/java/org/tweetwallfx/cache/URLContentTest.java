4c4567d68a3409eb71a6a2427bcd9da72c8c1900
Patrick Reinhart <patrick@reini.net>
2024-10-08 09:20:35 +0200

    Converted URLContent back to class
    Signed-off-by: Patrick Reinhart <patrick@reini.net>

--- cache/src/test/java/org/tweetwallfx/cache/URLContentTest.java
+++ cache/src/test/java/org/tweetwallfx/cache/URLContentTest.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2022 TweetWallFX
+ * Copyright (c) 2022-2024 TweetWallFX
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,17 +25,53 @@ package org.tweetwallfx.cache;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.tweetwallfx.cache.URLContent.NO_CONTENT;
 
 class URLContentTest {
+    static final String SERIALIZED_DATA = """
+            rO0ABXNyACBvcmcudHdlZXR3YWxsZnguY2FjaGUuVVJMQ29udGVudAAAAAAAAAAAAgACWwAEZGF0YXQAAl\
+            tCTAAGZGlnZXN0dAASTGphdmEvbGFuZy9TdHJpbmc7eHB1cgACW0Ks8xf4BghU4AIAAHhwAAAAFnRoaXMg\
+            aXMgc29tZSBkZW1vIGRhdGF0AEBmMjgyYjk3MzM3YmQ2N2VhZjIyZTgxNmQ4OTIzOWUzMTQ5MTBiNWJlMD\
+            AxOWUzZmU0YzQxNTg5NjM0Njg3Mjdl""";
+    static final String DIGEST = "f282b97337bd67eaf22e816d89239e314910b5be0019e3fe4c4158963468727e";
+    public static final byte[] TEST_DATA = "this is some demo data".getBytes(StandardCharsets.UTF_8);
+
     @Test
     void equals() throws IOException {
         assertThat(NO_CONTENT)
                 .isEqualTo(new URLContent(new byte[0], NO_CONTENT.digest()))
                 .isEqualTo(URLContent.of(InputStream.nullInputStream()));
+    }
+
+    @Test
+    void serialization() throws IOException {
+        URLContent content = URLContent.of(new ByteArrayInputStream(TEST_DATA));
+        ByteArrayOutputStream out= new ByteArrayOutputStream();
+        try (ObjectOutputStream objOout= new ObjectOutputStream(out)){
+            objOout.writeObject(content);
+        }
+        assertThat(content.digest()).isEqualTo(DIGEST);
+        assertThat(content.getInputStream()).hasBinaryContent(TEST_DATA);
+        assertThat(Base64.getEncoder().encodeToString(out.toByteArray())).isEqualTo(SERIALIZED_DATA);
+    }
+
+    @Test
+    void deserialization() throws IOException, ReflectiveOperationException {
+        try (ObjectInputStream objIn = new ObjectInputStream(new ByteArrayInputStream(Base64.getDecoder().decode(SERIALIZED_DATA)))) {
+            URLContent content = (URLContent)objIn.readObject();
+
+            assertThat(content.digest()).isEqualTo(DIGEST);
+            assertThat(content.getInputStream()).hasBinaryContent(TEST_DATA);
+        }
     }
 }
