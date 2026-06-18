11925f1f8cd5e00eaf71430860c366232d7dd6c6
Patrick Reinhart <patrick@reini.net>
2026-06-18 21:55:25 +0200

    Addressed error prone issues
    Signed-off-by: Patrick Reinhart <patrick@reini.net>

--- conference-api/src/main/java/org/tweetwallfx/conference/api/Room.java
+++ conference-api/src/main/java/org/tweetwallfx/conference/api/Room.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2022 TweetWallFX
+ * Copyright (c) 2022-2026 TweetWallFX
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -53,7 +53,7 @@ public interface Room extends Identifiable, Comparable<Room> {
 
     @Override
     public default int compareTo(final Room o) {
-        return Comparator.comparing(Room::getWeight)
+        return Comparator.comparingDouble(Room::getWeight)
                 .thenComparing(Room::getName)
                 .compare(this, o);
     }
